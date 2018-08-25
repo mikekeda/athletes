694fd2530da06942be80f89e7daecff1b705b3e3
Mykhailo Keda <mriynuk@gmail.com>
2018-08-25 16:50:37 +0300

    Improved representation for Marketability and Optimal campaign time.

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,19 @@
 $(document).ready( function () {
+    var months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+
     $(".button-collapse").sideNav();
 
     var table = $('#athletes-table').DataTable({
@@ -20,6 +35,20 @@ $(document).ready( function () {
             { "data": "market_transfer" },
             { "data": "instagram" },
             { "data": "twiter" },
+        ],
+        columnDefs: [
+            {
+                "render": function ( data, type, row ) {
+                    return '<i class="material-icons dp48 tiny orange-text lighten-3-text">star</i>'.repeat(parseInt(data));
+                },
+                "targets": 7
+            },
+            {
+                "render": function ( data, type, row ) {
+                    return months[data - 1];
+                },
+                "targets": 8
+            },
         ]
     });
 
