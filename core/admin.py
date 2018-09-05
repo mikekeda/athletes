07bb843d0330e588dff7cdcc3d509360db7c5ade
Mykhailo Keda <mriynuk@gmail.com>
2018-09-05 09:24:52 +0300

    Added admin action to update athletes info with data from Wikipedia.

--- core/admin.py
+++ core/admin.py
@@ -7,10 +7,19 @@ from core.models import Athlete
 AthleteForm = select2_modelform(Athlete)
 
 
+def update_data_from_wiki(_, __, queryset):
+    """ Update information for selected athletes with data from Wikipedia. """
+    for obj in queryset:
+        obj.get_data_from_wiki()
+        obj.save()
+update_data_from_wiki.short_description = "Update data from Wikipedia"
+
+
 class AthleteAdmin(ImportExportModelAdmin):
     list_filter = ('category',)
     search_fields = ['name']
     form = AthleteForm
+    actions = [update_data_from_wiki]
 
 
 admin.site.register(Athlete, AthleteAdmin)
