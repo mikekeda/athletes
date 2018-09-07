a2f37f2702eb91f439d2dfc45f0556373f96e513
Mykhailo Keda <mriynuk@gmail.com>
2018-09-07 10:11:27 +0300

    Improved parsing (case with FC_Lviv)

--- core/views.py
+++ core/views.py
@@ -225,9 +225,13 @@ class ParseTeamView(View):
             site = f'{site.scheme}://{site.hostname}'
             html = requests.get(wiki_url)
             soup = BeautifulSoup(html.content, 'html.parser')
-            title = soup.select("#Current_squad") or soup.select(
-                "#Current_roster") or soup.select("#Roster") or soup.select(
-                "#First-team_squad") or soup.select("#First_team_squad")
+            title = soup.select(
+                "#Current_squad") or soup.select(
+                "#Current_roster") or soup.select(
+                "#Roster") or soup.select(
+                "#First-team_squad") or soup.select(
+                "#First_team_squad") or soup.select(
+                "#Team_squad")
             form.cleaned_data['team'] = soup.title.string.split(
                 ' - Wikipedia')[0]
             table = title[0].parent.find_next_sibling("table")
