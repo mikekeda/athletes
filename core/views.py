7590dd6c19011b91160a19cb744b59d9cdd90693
Mykhailo Keda <mriynuk@gmail.com>
2018-09-08 18:08:26 +0300

    Improved parsing (case with Tala'ea El Gaish SC).

--- core/views.py
+++ core/views.py
@@ -260,7 +260,8 @@ class ParseTeamView(View):
                 "#Current_roster_and_coaching_staff") or soup.select(
                 "#First_Team_Squad") or soup.select(
                 "#Current_squad[11]") or soup.select(
-                "#Current_players")
+                "#Current_players") or soup.select(
+                "#Current_first_team_squad")
             form.cleaned_data['team'] = soup.title.string.split(
                 ' - Wikipedia')[0]
             table = title[0].parent.find_next_sibling("table")
