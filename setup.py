780da02a7c8d8e488cbd9efab9c3917e97428d51
Mykhailo Keda <mriynuk@gmail.com>
2018-11-17 10:04:37 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ setup(
     version='0.0.5',
     packages=find_packages(),
     install_requires=[
-        'requests==2.19.1',
-        'psycopg2==2.7.5',
-        'psycopg2-binary==2.7.5',
+        'requests==2.20.1',
+        'psycopg2==2.7.6.1',
+        'psycopg2-binary==2.7.6.1',
         'gunicorn==19.9.0',
         'beautifulsoup4==4.6.3',
         'celery==4.2.1',
         'requests_oauthlib==1.0.0',
         'Pillow==5.3.0',
-        'pytz==2018.6',
+        'pytz==2018.7',
 
-        'django==2.1.2',
+        'django==2.1.3',
         'django-widget-tweaks==1.4.3',
         'django-easy-select2==1.5.5',
         'django-import-export',  # Installed from git due the the bug
@@ -25,7 +25,7 @@ setup(
     ],
     extras_require={
         "test": [
-            'coverage==4.5.1',
+            'coverage==4.5.2',
         ]
     },
 )
