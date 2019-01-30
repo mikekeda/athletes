9c6d5149ec91099c285a3c61f13ef341b7076698
Mykhailo Keda <mriynuk@gmail.com>
2019-01-30 23:19:18 +0200

    Fixed setup.py

--- athletes/settings.py
+++ athletes/settings.py
@@ -190,7 +190,11 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'a.mkeda.me',
+    '127.0.0.1:8000',
+    '127.0.0.1:4200'
+)
 
 
 EMAIL_HOST = 'smtp.mailgun.org'
