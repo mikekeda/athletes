c70765f586795d4849f931ee4479012f8f34cfe5
Mykhailo Keda <mriynuk@gmail.com>
2019-03-20 19:20:53 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -10,28 +10,28 @@ setup(
         'psycopg2-binary==2.7.7',
         'gunicorn==19.9.0',
         'beautifulsoup4==4.7.1',
-        'celery==4.2.1',
+        'celery==4.2.2',
         'requests_oauthlib==1.2.0',
         'Pillow==5.4.1',
         'pytz==2018.9',
         'google-cloud-logging==1.10.0',
-        'python-dateutil==2.7.5',
-        'xmltodict==0.11.0',
+        'python-dateutil==2.8.0',
+        'xmltodict==0.12.0',
 
-        'django==2.1.5',
+        'django==2.1.7',
         'django-widget-tweaks==1.4.3',
         'django-easy-select2==1.5.5',
         'django-import-export',  # Installed from git due the the bug
         'django-debug-toolbar==1.11',
         'django-redis==4.10.0',
-        'djangorestframework==3.9.1',
-        'djangorestframework_simplejwt==3.3',
-        'dj-stripe==1.2.3',
-        'django-cors-headers==2.4.0',
+        'djangorestframework==3.9.2',
+        'djangorestframework_simplejwt==4.1.0',
+        'dj-stripe==2.0.0',
+        'django-cors-headers==2.5.2',
     ],
     extras_require={
         "test": [
-            'coverage==4.5.2',
+            'coverage==4.5.3',
         ]
     },
 )
